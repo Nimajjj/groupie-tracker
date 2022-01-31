a27f4da3d925bd2739b6846021439f69ed867a82
ben <benjamin.borello@ynov.com>
2022-01-31 09:54:25 +0100

    create base server

--- go/main.go
+++ go/main.go
@@ -1,6 +1,22 @@
 package main
 
+import (
+  "fmt"
+  "net/http"
+  "html/template"
+)
 
 func main() {
-  print("hello")
+  fmt.Println("Starting server -> localhost:80")
+
+  indexTemplate := template.Must(template.ParseFiles("../page/index.html"))
+
+  cssFolder := http.FileServer(http.Dir("../css"))
+  http.Handle("/css/", http.StripPrefix("/css/", cssFolder))
+
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    indexTemplate.Execute(w, nil)
+  })
+
+  http.ListenAndServe(":80", nil)
 }
